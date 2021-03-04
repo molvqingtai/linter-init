3d8d6d3b550e2ba0820c962e1775af0da5db20cb
JohnWu <molvqingtai@gmail.com>
2021-03-05 01:40:18 +0800

    ci: modify printWidth

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,10 @@
 
 // import inquirer from 'inquirer'
 import { Command } from 'commander'
-
 const program = new Command()
 
 void (async () => {
-  program.version(
-    'linter-init v0.0.1',
-    '-v, --version',
-    'Output linter-init version'
-  )
+  program.version('linter-init v0.0.1', '-v, --version', 'Output linter-init version')
   program.command('init').description('Initialize linter configuration')
   program.parse(process.argv)
 })().catch((error) => {
